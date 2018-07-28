4cfc4f176acdb85bbb968e7ca9e48430384433f1
Phillip Frantz <pfrantz@3-elecric-sheep.com>
2018-07-28 16:50:29 +1000

    update to license and copyright conment

--- WiSDK/Classes/TESApi.m
+++ WiSDK/Classes/TESApi.m
@@ -5,17 +5,18 @@
 //  Created by Phillp Frantz on 13/07/2017.
 //  Copyright © 2012-2018 3 Electric Sheep Pty Ltd. All rights reserved.
 //
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
+//  The Welcome Interruption Software Development Kit (SDK) is licensed to you subject to the terms
+//  of the License Agreement. The License Agreement forms a legally binding contract between you and
+//  3 Electric Sheep Pty Ltd in relation to your use of the Welcome Interruption SDK.
+//  You may not use this file except in compliance with the License Agreement.
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+//  A copy of the License Agreement can be found in the LICENSE file in the root directory of this
+//  source tree.
 //
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
+//  Unless required by applicable law or agreed to in writing, software distributed under the License
+//  Agreement is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//  express or implied. See the License Agreement for the specific language governing permissions
+//  and limitations under the License Agreement.
 
 #import "TESApi.h"
 #import "TESUtil.h"
