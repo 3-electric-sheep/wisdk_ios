06f29c3e0d2c1eefafb7b1b1a0d9b0b7fdc30648
Phillip Frantz <pfrantz@3-elecric-sheep.com>
2018-07-28 16:51:07 +1000

    update to license and copyright conment

--- Example/WiSDK/main.m
+++ Example/WiSDK/main.m
@@ -5,6 +5,18 @@
 //  Created by pfrantz on 07/20/2018.
 //  Copyright (c) 2018 pfrantz. All rights reserved.
 //
+//  The Welcome Interruption Software Development Kit (SDK) is licensed to you subject to the terms
+//  of the License Agreement. The License Agreement forms a legally binding contract between you and
+//  3 Electric Sheep Pty Ltd in relation to your use of the Welcome Interruption SDK.
+//  You may not use this file except in compliance with the License Agreement.
+//
+//  A copy of the License Agreement can be found in the LICENSE file in the root directory of this
+//  source tree.
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License
+//  Agreement is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//  express or implied. See the License Agreement for the specific language governing permissions
+//  and limitations under the License Agreement.
 
 @import UIKit;
 #import "WiAppDelegate.h"
